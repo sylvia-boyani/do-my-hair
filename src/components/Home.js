0a192135d0e66311adcca1d9cfe7a293d4b1aba9
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-09-09 11:07:16 +0300

    edit db functions

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,30 +2,30 @@
 import  './Home.css';
 import BookPage from './Book';
 import { useState } from 'react';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getDatabase, ref, set } from "firebase/database";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD5_v4FdF5LswBMz6sbhVUp5pHV0XXxB78",
-  authDomain: "hair-with-flair-237c8.firebaseapp.com",
-  databaseURL: "https://hair-with-flair-237c8-default-rtdb.firebaseio.com",
-  projectId: "hair-with-flair-237c8",
-  storageBucket: "hair-with-flair-237c8.appspot.com",
-  messagingSenderId: "1084069080219",
-  appId: "1:1084069080219:web:e24018370fbb0c619aea3e",
-  measurementId: "G-8MCPVDTLQH"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD5_v4FdF5LswBMz6sbhVUp5pHV0XXxB78",
+//   authDomain: "hair-with-flair-237c8.firebaseapp.com",
+//   databaseURL: "https://hair-with-flair-237c8-default-rtdb.firebaseio.com",
+//   projectId: "hair-with-flair-237c8",
+//   storageBucket: "hair-with-flair-237c8.appspot.com",
+//   messagingSenderId: "1084069080219",
+//   appId: "1:1084069080219:web:e24018370fbb0c619aea3e",
+//   measurementId: "G-8MCPVDTLQH"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const database = getDatabase(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const database = getDatabase(app);
 
 
 const Home = () => {
